c39c791e5bf019632b15be17feb51d5108ea2571
mohiturkude8237 <mohiturkude8237@gmail.com>
2025-06-25 22:49:31 +0530

    fav icon added

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,10 +2,12 @@ import Old from './old.jpg'
 import need from './need.jpg'
 import children from './children.png'
 import women from './women.jpg'
+import fav from './fav.png'
 export const assets=
 {
     Old,
     need,
     children,
     women,
+    fav,
 }
